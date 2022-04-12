a8eedaec2764f5f8a42dd9a57bbbdeb9b0ec628d
Pallavi Suryawanshi <Pallavi.Suryawanshi@BRILLIO.COM>
2022-04-12 12:56:29 +0530

    Second commit

--- src/server.js
+++ src/server.js
@@ -5,6 +5,11 @@ const TodoRouter = require("./routes/todo.routes");
 const app = express();
 
 app.use(express.json());
+app.use(express.static(__dirname+"/public"));
+
+app.get("/", ( req , res ) => {
+    return res.sendFile(__dirname+"/public/index.js")
+})
 
 app.use("/api/todos", TodoRouter);
 
